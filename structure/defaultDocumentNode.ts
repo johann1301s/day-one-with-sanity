45768111e1a3e5b97bd1740ce49f8e40d30084ca
Johannes Skuterud <johann1301s@icloud.com>
2024-10-07 12:33:28 +0200

    wip

--- structure/defaultDocumentNode.ts
+++ structure/defaultDocumentNode.ts
@@ -1,6 +1,15 @@
 import type {DefaultDocumentNodeResolver} from 'sanity/structure'
 import DocumentsPane from 'sanity-plugin-documents-pane'
 import { PreviewComponent } from '../schemaTypes/components/PreviewComponent'
+import { Iframe } from 'sanity-plugin-iframe-pane'
+import { SanityDocument } from 'sanity'
+
+// Customise this function to show the correct URL based on the current document
+function getPreviewUrl(doc: any) {
+  return doc?.slug?.current
+    ? `http://localhost:3000/${doc.slug.current}`
+    : 'http://localhost:3000'
+}
 
 export const defaultDocumentNode: DefaultDocumentNodeResolver = (S, {schemaType}) => {
   switch (schemaType) {
@@ -29,6 +38,12 @@ export const defaultDocumentNode: DefaultDocumentNodeResolver = (S, {schemaType}
             options: { perspective: 'previewDrafts' }
           })
           .title('Other albums'),
+        S.view
+          .component(Iframe)
+          .options({
+            url: (doc: SanityDocument) => getPreviewUrl(doc),
+          })
+          .title('Preview Url'),
         S.view.component(PreviewComponent).title('Preview'),
       ])
     default:
